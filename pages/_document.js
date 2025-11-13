245e0d504e29105cad21e11a7f0f6f11983e661e
Saedakhram <102800804+Saedddd@users.noreply.github.com>
2025-11-13 14:23:40 +0600

    init: project

---
+++ pages/_document.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import data from '../data.json'
+const { name } = data
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" style={{ overflowX: "hidden" }}>
+        <Head>
+          <meta charSet='utf-8' />
+          {/* PWA primary color */}
+          <meta name="theme-color" content="black" />
+          <meta name="description" content={`Portfolio of ${name}`}/>
+          <meta name='keywords' content={'Portfolio ' + name + ' skills projects experience resume'} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/manifest.webmanifest"></link>
+  
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+
+MyDocument.getInitialProps = async (ctx) => {
+
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
